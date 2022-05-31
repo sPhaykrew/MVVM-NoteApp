8ae6fa7c136ab390903a7e28de7a407b2097557c
Sakda Phaykrew <s_phaykrew@hotmail.com>
2022-05-31 16:37:36 +0700

    add search notes

--- app/src/main/java/com/example/note/view/MainFragment.kt
+++ app/src/main/java/com/example/note/view/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.example.note.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,15 +14,19 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.note.R
 import com.example.note.databinding.FragmentMainBinding
 import com.example.note.model.NoteDB
+import com.example.note.model.NoteData
 import com.example.note.model.NoteRepository
 import com.example.note.viewmodel.NoteViewModel
 import com.example.note.viewmodel.NoteViewModelFactory
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainFragment : Fragment() {
 
-    private lateinit var binding : FragmentMainBinding
+    private lateinit var binding: FragmentMainBinding
     private lateinit var noteViewModel: NoteViewModel
     private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    private lateinit var notes: List<NoteData>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +37,8 @@ class MainFragment : Fragment() {
         initViewModel()
         initRecyclerView()
 
-        noteViewModel.getNote.observe(viewLifecycleOwner, Observer{
+        noteViewModel.getNote.observe(viewLifecycleOwner, Observer {
+            notes = it
             recyclerViewAdapter.setItem(it)
             binding.recyclerview.adapter = recyclerViewAdapter
         })
@@ -40,20 +47,45 @@ class MainFragment : Fragment() {
             findNavController().navigate(R.id.action_mainFragment_to_addNoteFragment)
         }
 
+        binding.search.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                noteFilter(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+
         return binding.root
     }
 
-        private fun initViewModel(){
+    private fun initViewModel() {
         val dao = NoteDB.getDatabase(requireActivity().application).NoteDAO()
         val repository = NoteRepository(dao)
         val factory = NoteViewModelFactory(repository)
-        noteViewModel = ViewModelProvider(requireActivity(),factory)[NoteViewModel::class.java]
+        noteViewModel = ViewModelProvider(requireActivity(), factory)[NoteViewModel::class.java]
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         recyclerViewAdapter = RecyclerViewAdapter()
-//        binding.recyclerview.adapter = recyclerViewAdapter
-        binding.recyclerview.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+        binding.recyclerview.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+    }
+
+    private fun noteFilter(text : String){
+        val filter = ArrayList<NoteData>()
+        for (note in notes) {
+            if (note.title.contains(text) || note.content.contains(text)) {
+                filter.add(note)
+            }
+        }
+        recyclerViewAdapter.setItem(filter)
+        recyclerViewAdapter.notifyDataSetChanged()
     }
 
 }
